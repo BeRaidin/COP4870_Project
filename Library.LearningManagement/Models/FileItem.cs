d78c1ad84201ea16f8a0fa89a773a5984fa4dac5
BeRaidin <braydenclewis@gmail.com>
2023-02-24 17:00:26 -0500

    Added derived class for PageItem, AssignmentItem, FileItem. Closes #30 Closes #31 Closes #32

---
+++ Library.LearningManagement/Models/FileItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.LearningManagement.Models
+{
+    public class FileItem : ContentItem
+    {
+        public string? FilePath { get; set; }
+
+        public FileItem()
+        {
+            FilePath = string.Empty;
+        }
+    }
+}
