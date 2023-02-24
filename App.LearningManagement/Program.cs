362f3a5bd2a7caa1e353de33daf789a6b4c190e7
BeRaidin <braydenclewis@gmail.com>
2023-02-24 12:59:50 -0500

    Updated menu to Switch instead of if else.

--- App.LearningManagement/Program.cs
+++ App.LearningManagement/Program.cs
@@ -15,36 +15,50 @@ namespace MyApp // Note: actual namespace depends on the project name.
             while (cont)
             {
                 Console.WriteLine("Choose an option:");
-                Console.WriteLine("1. Add a student enrollment");
-                Console.WriteLine("2. Update a student enrollment");
-                Console.WriteLine("3. List all enrolled students");
-                Console.WriteLine("4. Search for student");
                 Console.WriteLine("5. Add a new course");
-                Console.WriteLine("6. Update a course");
+                Console.WriteLine("1. Add a student enrollment");
                 Console.WriteLine("7. List all courses");
+                Console.WriteLine("3. List all enrolled students");
                 Console.WriteLine("8. Search for courses");
+                Console.WriteLine("4. Search for student");
+                Console.WriteLine("6. Update a course");
+                Console.WriteLine("2. Update a student enrollment");
                 Console.WriteLine("9. Exit");
                 var input = Console.ReadLine();
-                if (int.TryParse(input, out int result)) {
-                    if (result == 1)
-                        studentHelper.AddOrUpdateStudent();
-                    else if (result == 2)
-                        studentHelper.UpdateStudentRecord();
-                    else if (result == 3)
-                        studentHelper.ListStudents();
-                    else if (result == 4)
-                        studentHelper.SearchStudents();
-                    else if (result == 5)
-                        courseHelper.AddOrUpdateCourse();
-                    else if (result == 6)
-                        courseHelper.UpdateCourseRecord();
-                    else if (result == 7)
-                        courseHelper.ListCourses();
-                    else if (result == 8)
-                        courseHelper.SearchCourses();
-                    else if (result == 9)
-                        cont = false;
-                }
+
+                if (int.TryParse(input, out int result))
+                    switch (result)
+                    {
+                        case 1:
+                            courseHelper.AddOrUpdateCourse();
+                            break;
+                        case 2:
+                            studentHelper.AddOrUpdateStudent();
+                            break;
+                        case 3:
+                            courseHelper.ListCourses();
+                            break;
+                        case 4:
+                            studentHelper.ListStudents();
+                            break;
+                        case 5:
+                            courseHelper.SearchCourses();
+                            break;
+                        case 6:
+                            studentHelper.SearchStudents();
+                            break;
+                        case 7:
+                            courseHelper.UpdateCourseRecord();
+
+                            break;
+                        case 8:
+                            studentHelper.UpdateStudentRecord();
+                            break;
+                        case 9:
+                            cont = false;
+                            break;
+                    }
+                
             }
         }
     }
