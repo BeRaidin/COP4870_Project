87238dc3f5fa3fb36905caf0ca567bd58aad8e5f
BeRaidin <braydenclewis@gmail.com>
2023-03-27 14:18:07 -0400

    Created ability to assign type of person to a person and grade level to a student. Closes #102

---
+++ UWP.LearningManagement/BooleanToVisibilityConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml;
+using Library.LearningManagement.Services;
+using Library.LearningManagement.Models;
+using UWP.LearningManagement.Dialogs;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+using Windows.UI.Xaml.Controls;
+using Windows.Foundation.Collections;
+using System.Reflection.Metadata;
+
+namespace UWP.LearningManagement
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isVisible = (bool)value;
+            return isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- UWP.LearningManagement/ViewModels/DetailedPersonViewModel.cs
+++ UWP.LearningManagement/ViewModels/DetailedPersonViewModel.cs
@@ -31,11 +31,59 @@ namespace UWP.LearningManagement.ViewModels
             get { return personService.CurrentPerson.Courses; }
         }
 
+        public string Type { get; set; }
+        public string GradeLevel { get; set; }
+
         public DetailedPersonViewModel()
         {
             courseService = CourseService.Current;
             personService = PersonService.Current;
+            SetType();
+            SetGradeLevel();
         }
+
+        public void SetType()
+        {
+            if (personService.CurrentPerson as Student != null) 
+            {
+                Type = "Student";
+            }
+            else if (personService.CurrentPerson as Instructor != null)
+            {
+                Type = "Instructor";
+            }
+            else if (personService.CurrentPerson as TeachingAssistant != null)
+            {
+                Type = "Teaching Assistant";
+            }
+        }
+
+        public void SetGradeLevel()
+        {
+            var student = personService.CurrentPerson as Student;
+            if (student != null)
+            {
+                if (student.Classification == Student.Classes.Freshman)
+                {
+                    GradeLevel = "Freshman";
+                }
+                else if (student.Classification == Student.Classes.Sophmore)
+                {
+                    GradeLevel = "Sophmore";
+                }
+                else if (student.Classification == Student.Classes.Junior)
+                {
+                    GradeLevel = "Junior";
+                }
+                else if (student.Classification == Student.Classes.Senior)
+                {
+                    GradeLevel = "Senior";
+                }
+                else GradeLevel = "";
+            }
+        }
+
+
 
 
     }
