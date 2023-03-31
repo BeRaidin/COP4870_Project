8e771fc32f97704cf36d9555aa7a3ef2adbfefc6
BeRaidin <braydenclewis@gmail.com>
2023-03-31 15:23:25 -0400

    Instructor can now give grades to only the selected course's assignments. Closes #150

---
+++ UWP.LearningManagement/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Library.LearningManagement.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagement.ViewModels
+{
+    public class MainPageViewModel
+    {
+        private readonly PersonService personService;
+        private readonly ModuleService moduleService;
+        private readonly CourseService courseService;
+
+        public MainPageViewModel() 
+        { 
+            personService = PersonService.Current;
+            moduleService = ModuleService.Current;
+            courseService = CourseService.Current;
+        }
+
+        public void Clear()
+        {
+            personService.CurrentPerson = null;
+            personService.CurrentAssignment = null;
+            moduleService.CurrentModule = null;
+            moduleService.CurrentItem = null;
+            courseService.CurrentCourse = null;
+        }
+    }
+}
